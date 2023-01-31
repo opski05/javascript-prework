e242165f20f108ebde2d97a8d1bd641dbddfdb9c
Oskar Pozorski <oskar.pozorski@gmail.com>
2023-01-31 18:21:45 +0100

    Add computerMove

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+
+if(randomNumber == 2){
+   computerMove = 'papier';
+}
+
+if(randomNumber == 2){
+   computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
@@ -5,15 +25,15 @@ console.log('Gracz wpisał: ' + playerInput);
 let playerMove = 'nieznany ruch';
 
 if(playerInput == '1'){
-  playerMove = 'kamień';
+   playerMove = 'kamień';
 }
 
 if(playerInput == '2'){
-	playerMove = 'papier';
-  }
+   playerMove = 'papier';
+}
 
  if(playerInput == '3'){
-	playerMove = 'nożyce';
-  }
+   playerMove = 'nożyce';
+}
 
-  printMessage('Twój ruch to: ' + playerMove);
+printMessage('Twój ruch to: ' + playerMove);
