7abadbec0e1d926ef48624e50c9ab26886032d3c
Oskar Pozorski <oskar.pozorski@gmail.com>
2023-01-31 18:04:42 +0100

    Add computerMove and playerMove to functions.js

--- js/script.js
+++ js/script.js
@@ -2,14 +2,11 @@ function printMessage(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
+	let computerMove = 'kamień';
+	let playerMove = 'papier';
+	printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
 }
 
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
-
-let computerMove = 'kamień';
-
-let playerMove = 'papier';
-
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
