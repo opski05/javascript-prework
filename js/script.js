34ac0f1f3d7ed4827961409328aa55b47b5de7f3
Oskar Pozorski <oskar.pozorski@gmail.com>
2023-02-04 16:32:31 +0100

    Remove whitespaces in Listener

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+{
 function playGame(playerInput) {
 	clearMessages()
 	function getMoveName(argMoveId) {
@@ -72,14 +73,9 @@ function playGame(playerInput) {
 	}
 }
 
-document.getElementById('play-rock').addEventListener('click', function () {
-	playGame('1');
-});
+document.getElementById('play-rock').addEventListener('click', function () {playGame('1')});
 
-document.getElementById('play-paper').addEventListener('click', function () {
-	playGame('2');
-});
+document.getElementById('play-paper').addEventListener('click', function () {playGame('2')});
 
-document.getElementById('play-scissors').addEventListener('click', function () {
-	playGame('3');
-});
+document.getElementById('play-scissors').addEventListener('click', function () {playGame('3')});
+}
