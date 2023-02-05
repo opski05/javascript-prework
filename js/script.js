892901cb1f1b869a8b8d75d3fb272c5600a8fa0b
Oskar Pozorski <oskar.pozorski@gmail.com>
2023-02-05 19:28:41 +0100

    code correction

--- js/script.js
+++ js/script.js
@@ -12,32 +12,9 @@
 		}
 	}
 
-	console.log('Wylosowana liczba to: ' + randomNumber);
-
-	if (randomNumber == 1) {
-		computerMove = 'kamień';
-	} else if (randomNumber == 2)
-		computerMove = 'papier';
-	else if (randomNumber == 3)
-		computerMove = 'nożyce';
-
-	printMessage('Mój ruch to: ' + computerMove);
-
-	/*let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');*/
-
-	console.log('Gracz wpisał: ' + playerInput);
-
-	/*if(playerInput == '1'){
-	   playerMove = 'kamień';
-	} else if(playerInput == '2')
-		playerMove = 'papier';
-	  else if(playerInput == '3')
-		playerMove = 'nożyce';*/
-
-	printMessage('Twój ruch to: ' + playerMove);
-
-	const displayResult = function (argComputerMove, argPlayerMove) {
-		clearMessages()
+	const displayResult = function(argComputerMove, argPlayerMove) {
+		clearMessages();
+		printMessage('Komputer wybrał ' + argComputerMove + ', Ty wybierasz ' + argPlayerMove + '.');
 		if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
 			printMessage('Ty wygrywasz!');
 		} else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
